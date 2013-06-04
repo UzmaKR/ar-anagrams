de8d7f57567ab46a7cd165e856d958e96e88aba0
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-04 09:00:01 -0700

    keyword lookup

--- public/js/application.js
+++ public/js/application.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
 
+
+
+
   $("form").submit(function(e) {
     e.preventDefault();
+    $("form").keyup(_.debounce(function() { 
     $.ajax({
       type: 'get',
       url: '/ghost',
       data: $("form").serialize()
-    }).done(function(data) {
-      $('.word').children('p').remove();
-      var array = $.parseJSON(data);
-      $.each(array, function(index, value){
-        $('.word').append("<p>"+value+"</p>");
+      }).done(function(data) {
+        $('.word').children('p').remove();
+        var array = $.parseJSON(data);
+        $.each(array, function(index, value){
+          $('.word').append("<p>"+value+"</p>");
+        });
       });
-    });
+    }));
   });
 });
