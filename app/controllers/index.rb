0cecd62218f38b98cfd89174d5d5be60d6e498fd
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-04 00:06:05 -0700

    enabled search bar

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,10 @@
-get '/:word' do
-  @word = params[:word]
-  @results = Word.anagrams(@word)
+require 'debugger'
+
+get '/' do
   erb :index
 end
+
+get '/ghost' do
+  @user_input = params[:user_input]
+  Word.anagrams(@user_input).map! { |w| w.word }.to_json
+end 
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $("form").submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'get',
+      url: '/ghost',
+      data: $("form").serialize()
+    }).done(function(data) {
+      $('.word').children('p').remove();
+      var array = $.parseJSON(data);
+      $.each(array, function(index, value){
+        $('.word').append("<p>"+value+"</p>");
+      });
+    });
+  });
 });
